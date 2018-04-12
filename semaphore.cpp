29ec74776325a6d047340fed195a8de694723224
MahtaMR <zohreh.torabian7366@gmail.com>
2018-04-12 21:00:17 +0400

    modification

--- semaphore.cpp
+++ semaphore.cpp
@@ -1,71 +1,56 @@
-#include <iostream>
-#include <thread>
-#include <unistd.h>
-#include <mutex>
-#include <condition_variable>
 
-// Primitive Semaphore
-class Semaphore {
-private:
-  std::mutex mtx;
-  std::condition_variable conditionVariable;
-  // count is shared and should be protected
-  mutable int count;
+  // Primitive Semaphore
+  class Primitive_Semaphore {
+  private:
+    std::mutex mtx;
+    std::condition_variable conditionVariable;
+    // count is shared and should be protected
+    mutable int count;
 
-public:
-  explicit Semaphore(const int count)
-  : count(count) {};
+  public:
+    explicit Semaphore(const int count)
+    : count(count) {};
 
-  void sem_wait() {
-    std::unique_lock<std::mutex> locker(mtx);
-    this->count--;
-    if (this->count < 0) {
-      // block the process
-      conditionVariable.wait(locker);
+    void sem_wait() {
+      std::unique_lock<std::mutex> locker(mtx);
+      this->count--;
+      if (this->count < 0) {
+        // block the process
+        conditionVariable.wait(locker);
+      }
+      // ...
     }
-    // ...
-  }
 
-  void sem_signal() {
-    std::unique_lock<std::mutex> locker(mtx);
-    this->count++;
-    if (this->count <= 0) {
-      // unblock a process
-      conditionVariable.notify_one();
+    void sem_signal() {
+      std::unique_lock<std::mutex> locker(mtx);
+      this->count++;
+      if (this->count <= 0) {
+        // unblock a process
+        conditionVariable.notify_one();
+      }
+      // ...
     }
-    // ...
-  }
-
-  int getCount() {
-    return this->count;
-  }
-};
-
-// thread one function
-void threadone(Semaphore* semaphore) {
-  for (unsigned int i = 0; i < 7; i++) {
-    semaphore->sem_wait();
-    sleep(5);
   };
-}
 
+  // Binary Semaphore
+  class Binary_Semaphore {
+  private:
+    std::mutex mtx;
+    std::condition_variable conditionVariable;
+    // count is shared and should be protected
+    enum Value {
+      zero = 0, one = 1
+    };
 
-// thread two function
-void threadtwo(Semaphore* semaphore) {
-  for (unsigned int i = 0; i < 2; i++) {
-    semaphore->sem_signal();
-    sleep(5);
+  public:
+
+    void sem_wait() {
+      std::unique_lock<std::mutex> locker(mtx);
+    
+    }
+
+    void sem_signal() {
+      std::unique_lock<std::mutex> locker(mtx);
+
+    }
   };
-}
-
-int main() {
-  Semaphore* semaphore = new Semaphore(5);
-
-  std::thread thread_1(threadone, semaphore);
-  std::thread thread_2(threadtwo, semaphore);
-
-  thread_1.join();
-  thread_2.join();
-
-  // ...
-}
